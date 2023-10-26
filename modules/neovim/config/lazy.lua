e2548f9ab0eaa9c604eea4285aa124ba61206b9d
Justin Gardner <github.frugally996@passmail.net>
2023-10-25 18:58:56 -0600

    Add Flake8 Linting

--- modules/neovim/config/lazy.lua
+++ modules/neovim/config/lazy.lua
@@ -92,6 +92,29 @@ require("lazy").setup({
         })
       end,
     },
+    {
+      "mfussenegger/nvim-lint",
+      event = {
+        "BufReadPre",
+        "BufNewFile",
+      },
+      config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+          python = { "flake8" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+          group = lint_augroup,
+          callback = function()
+            lint.try_lint()
+          end,
+        })
+      end,
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
